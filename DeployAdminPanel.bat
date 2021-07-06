3c31163aa765f80220fbbd804f8f41ffcd4aa962
Saeed Nemati <sn.1361@gmail.com>
2021-07-06 11:18:27 +0430

    unnecessary

--- DeployAdminPanel.bat
+++
@@ -1,3 +0,0 @@
-%HolismProjectsRoot%\Quality\Deployer\bin\Holism.Deployer.exe %SaeedProjectsRoot%\NefcantoWeb\DeployAdminPanel.json
-
-pause
--- DeployPublicPanel.bat
+++
@@ -1,3 +0,0 @@
-%HolismProjectsRoot%\Quality\Deployer\bin\Holism.Deployer.exe %SaeedProjectsRoot%\NefcantoWeb\DeployPublicPanel.json
-
-pause
