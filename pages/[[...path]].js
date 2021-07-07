fba91c750caf4be57af3cb427dd9ec20daec92fa
Saeed Nemati <sn.1361@gmail.com>
2021-07-07 10:16:41 +0430

    almost works

--- pages/[[...path]].js
+++ pages/[[...path]].js
@@ -1,41 +1,55 @@
 const fs = require('fs');
 const path = require('path');
+import Error from 'next/error'
 
-const index = (props) => {
-    const path = props.path;
+const index = ({ urlSegments, content, errorCode }) => {
 
-    return (
-        <>
-            {
-                path.length == 0
-                    ?
-                    <div>home page</div>
-                    :
-                    path?.map(item => <div key={item}>{item}</div>)
-            }
-        </>
-    );
+    if (errorCode) {
+        return <Error statusCode={errorCode} />
+    }
+
+    if (urlSegments.length == 0) {
+        return <div>home page</div>
+    }
+
+    return <>
+        {
+            urlSegments?.map(item =>
+                <div key={item}>{item}</div>
+            )
+        }
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+    </>
 }
 
-export async function getServerSideProps(context) {
-    const urlPaths = context.params.path || [];
-    var filePath = path.join.apply(null, [...urlPaths, 'index.html']);
+export async function getServerSideProps({ params, res }) {
+    const urlSegments = params.path || [];
+    const diskSegments = [process.cwd(), 'contents'].concat(urlSegments);
+    var filePath = path.join.apply(null, [...diskSegments, 'index.html']);
+    console.log(filePath);
     if (!fs.existsSync(filePath)) {
-        filePath = path.join.apply(null, [...urlPaths, '.md']);
+        filePath = path.join.apply(null, [...diskSegments]) + '.md';
     }
     if (!fs.existsSync(filePath)) {
-        filePath = path.join.apply(null, [...urlPaths, 'index.html']);
+        filePath = path.join.apply(null, [...diskSegments]) + '.html';
     }
-    const content = "";
-    fs.readFileSync(__dirname + '/../contents/' + filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
+    if (!fs.existsSync(filePath)) {
+        res.statusCode = 404;
+        const result = { props: { urlSegments: urlSegments, errorCode: 404 } }
+        return result;
+    }
+    else {
+        try {
+            var content = fs.readFileSync(filePath, 'utf8');
+            const result = { props: { urlSegments: urlSegments, content: content } };
+            return result;
+        } catch (e) {
+            console.log(e);
+            res.statusCode = 500;
+            const result = { props: { urlSegments: urlSegments, errorCode: 500 } }
+            return result;
         }
-        else {
-            content = data;
-        }
-    });
-    return { props: { path: urlPaths, content: contnet } }
+    }
 }
 
 export default index;
