e68b9e4587c08373f2b36a9c954be1f61311bb70
Saeed Nemati <sn.1361@gmail.com>
2021-07-07 15:21:51 +0430

    title working now

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <title>{process.env.BRAND} - {process.env.SLOGAN}</title>
+                <title>{process.env.NEXT_PUBLIC_BRAND} - {process.env.NEXT_PUBLIC_SLOGAN}</title>
                 <link rel="icon" href="/images/favicon.ico" />
             </Head>
             <Component {...pageProps} />
